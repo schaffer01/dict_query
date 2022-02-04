1278577ec4331e71ae12f63cd038b865a6167828
zhang <3257194259@qq.com>
2022-02-04 22:43:37 +0800

    数据库创建

---
+++ insert_word_into_mysql-dict-words.py
@@ -0,0 +1,21 @@
+import re, pymysql
+
+db = pymysql.connect(host='127.0.0.1', port=3306,
+                     user='root', password='123456',
+                     database='dict', charset='utf8')
+cur = db.cursor()
+f = open('dict.txt', 'r')
+args_list = []
+for line in f:
+    tup = re.findall(r'(\w+)\s+(.*)', line)
+    args_list.append(tup)
+f.close()
+sql = "insert into words(word,mean)values(%s,%s);"
+try:
+    cur.executemany(sql,args_list)
+    db.commit()
+except Exception as e:
+    print(e)
+    db.rollback()
+cur.close()
+db.close()
